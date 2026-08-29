2d1b2968dc6bb2cb53ed58cb99cdd93aeedd996c
AbdullahAbuZaid04 <abdullahabuzaid2004@gmail.com>
2026-08-29 15:44:00 +0300

    added tailwind CSS and implement Button Component

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,22 @@
-export default function Button({ children, onClick, variant = "primary" }) {
+export default function Button({
+  text,
+  onClick,
+  variant = "primary",
+  disabled,
+}) {
+  const buttonStyles = {
+    primary:
+      "border border-blue-500 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600",
+    secondary:
+      "border border-gray-300 bg-gray-300 text-gray-500 px-4 py-2 rounded cursor-not-allowed",
+  };
   return (
-    <button onClick={onClick} className={`btn btn-${variant}`}>
-      {children}
+    <button
+      onClick={onClick}
+      className={`${buttonStyles[variant]} cursor-pointer`}
+      disabled={disabled}
+    >
+      {text}
     </button>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,24 @@
+import Button from "../components/ui/Button";
+
 export default function Home() {
   return (
-    <div className="page home-page">
-      <h1>Welcome to Home Page</h1>
-      <p>This is the home page of our application.</p>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center gap-8">
+      <h1 className="text-3xl font-bold">Welcome to Home Page</h1>
+      <p className="text-lg">This is the home page of our application.</p>
+      <div className="flex gap-4">
+        <Button
+          text="Click Me"
+          onClick={() => alert("Button Clicked!")}
+          variant="primary"
+          disabled={false}
+        />
+        <Button
+          text="Click Me"
+          onClick={() => alert("Button Clicked!")}
+          variant="secondary"
+          disabled={true}
+        />
+      </div>
     </div>
   );
 }
